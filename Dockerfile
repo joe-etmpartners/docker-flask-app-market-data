55833feaaf6b73b10d7ad000a22856ca5648b8ca
joe-etmpartners <joe@etmpartnersllc.com>
2023-12-02 16:31:29 -0500

    logging working

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
 FROM python:3
 WORKDIR /app
 COPY . .
+# mount the secret in the correct location, then run pip install
+RUN --mount=type=secret,id=my_secret,required cat /run/secrets/my_secret > /app/secrets.txt
+RUN --mount=type=secret,id=aws_public_key cat /run/secrets/aws_public_key > /app/aws_public_key
+RUN --mount=type=secret,id=aws_private_key cat /run/secrets/aws_private_key > /app/aws_private_key
 RUN python3 -m pip install --upgrade pip
 RUN python3 -m pip install --no-cache-dir -r requirements.txt
 ENV FLASK_APP=mypython.py
 ENV FLASK_ENV=development
 ENV FLASK_RUN_HOST=0.0.0.0
-ENV [variable-name]=[default-value]
-ENV AWS_DEFAULT_REGION: "us-east-2b"
-ENV AWS_ACCESS_KEY_ID: "AKIARD3WOPAPDTY4N6F7"
-ENV AWS_SECRET_ACCESS_KEY: "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"
 EXPOSE 5000
 CMD ["flask", "run"]
--- mypython.py
+++ mypython.py
@@ -5,7 +5,7 @@ import watchtower
 
 from app import app
 
-
+#
 def init_logging(logfile, loglevel):
     """
     Defines the logging destination, level and format.
@@ -31,14 +31,68 @@ def init_logging(logfile, loglevel):
     formatter = logging.Formatter(logging_format)
     cw_handler.setFormatter(formatter)
     logger.error('ERROR MESSAGE #1')
+    logger.error('This is a new message sent after 9pm')
+    return cw_handler
     
+    # Force push the logs to CloudWatch
+    cw_handler.flush()
     try:
         x = 1 / 0
     except ZeroDivisionError:
         logger.exception('EXCEPTION log msg')  # logs at same level as logging.error() but includes traceback
     
+import os
 
-   
+secret_fpath = f'/app/aws_public_key'
+existence = os.path.exists(secret_fpath)
+if existence:
+    v2 = open(secret_fpath).read().rstrip('\n')
+    print("AWS public key file exists and contains: " + v2)
+    os.environ['AWS_ACCESS_KEY_ID'] = v2
+else:
+    print("AWS public key file does not exist")
+
+secret_fpath = f'/app/aws_private_key'
+existence = os.path.exists(secret_fpath)
+if existence:
+    v2 = open(secret_fpath).read().rstrip('\n')
+    print("AWS private key file exists and contains: " + v2)
+    os.environ['AWS_SECRET_ACCESS_KEY'] = v2
+else:
+    print("AWS private key file does not exist")
+#
+
+print("THIS SHOULD PRINT TO DOCKER LOG")
+sys.stdout.flush()  
+print (f"Name = {__name__}")
+sys.stdout.flush() 
+
+secret_fpath = f'/app/secrets.txt'
+existence = os.path.exists(secret_fpath)
+#FUDGE
+
+    
+if existence:
+    v2 = open(secret_fpath).read().rstrip('\n')
+    print("Secret file exists and contains: " + v2)
+else:
+    print("Secret file does not exist")
+
+
+
+for name, value in os.environ.items():
+    print("{0}: {1}".format(name, value))
+sys.stdout.flush() 
+
+print("Starting mypython.py")
+sys.stdout.flush()
+cw_handler=init_logging('', 0)
+logger = logging.getLogger('awslogger')
+logger.debug('DEBUG log msg #2')
+logger.error('ERROR MESSAGE A1')
+cw_handler.flush()
+logger.error('ERROR MESSAGE A2')
+logger.debug('DEBUG log msg B1')
 
 if __name__ == "__main__":
     #pass
@@ -47,6 +101,8 @@ if __name__ == "__main__":
     #                               https://docs.python.org/3/library/logging.html
     #                               https://docs.python.org/3/library/logging.html#logging-levels
 
+    print("Starting mypython.py")
+    sys.stdout.flush()
     init_logging('', 0)
 
     logger = logging.getLogger('awslogger')
