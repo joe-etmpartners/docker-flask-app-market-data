c69ce109d9029ee854128ed3d84bd3149d152604
joe-etmpartners <joe@etmpartnersllc.com>
2023-12-02 22:10:10 -0500

    run via api test working

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,45 @@
 from app import app
 from flask import request
+from ETMProcessManager import ETMProcessManager
+from ETMProcessManager import ETMProcess
+import rds_skywalker
+from time import sleep
 
 #  kosong
+
+def getLastDate(thisProcess):
+    thisProcess.set_status('Running')
+    thisProcess.set_progress(0)
+    sleep(3)
+
+    thisProcess.set_progress(10)
+    rdsCalendar = rds_skywalker.RDSCalendar()
+    thisProcess.set_progress(50)
+    rtn = rdsCalendar.getLastDate()
+    sleep(3)
+
+    thisProcess.set_progress(100)
+
+    thisProcess.set_status('Finishing')
+    sleep(3)
+    return rtn
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    pm = ETMProcessManager()
+    rtn = pm.get_all_process_info()
+    return rtn
+
+@app.route('/run')
+def run():
+    pm = ETMProcessManager()
+    process1 = ETMProcess('GetLastDate', 'Get last date from database', getLastDate)
+    pm.register_process(process1)
+    process1.run()
+    rtn = pm.get_all_process_info()
+    return rtn
+
 
 @app.route('/add')
 def add():
--- mypython.py
+++ mypython.py
@@ -11,55 +11,39 @@ import rds_skywalker
 
 rds_skywalker.printVersion()
 
-def getLastDate(thisProcess):
-    thisProcess.set_status('Running')
-    thisProcess.set_progress(0)
-    sleep(3)
 
-    thisProcess.set_progress(10)
-    rdsCalendar = rds_skywalker.RDSCalendar()
-    thisProcess.set_progress(50)
-    rtn = rdsCalendar.getLastDate()
-    sleep(3)
-
-    thisProcess.set_progress(100)
-
-    thisProcess.set_status('Finishing')
-    sleep(3)
-    return rtn
 
 etmLogger = ETM_AWS_Logger(1, 'MarketDataService')
 
 etmLogger.log('Starting mypython.py')
 
-rdsCalendar = rds_skywalker.RDSCalendar()
-rtn = rdsCalendar.getLastDate()
-print ('last date: ' + str(rtn))
-sys.stdout.flush() 
+# rdsCalendar = rds_skywalker.RDSCalendar()
+# rtn = rdsCalendar.getLastDate()
+# print ('last date: ' + str(rtn))
+# sys.stdout.flush() 
 
-pm = ETMProcessManager()
-process1 = ETMProcess('GetLastDate', 'Get last date from database', getLastDate)
+# pm = ETMProcessManager()
+# process1 = ETMProcess('GetLastDate', 'Get last date from database', getLastDate)
+# pm.register_process(process1)
 
-pm.register_process(process1)
+# print('All process info: {}'.format(pm.get_all_process_info()))
+# sys.stdout.flush() 
 
-print('All process info: {}'.format(pm.get_all_process_info()))
-sys.stdout.flush() 
+# sleep(1)
 
-sleep(1)
+# print('Process 1 info: {}'.format(process1.info()))
 
-print('Process 1 info: {}'.format(process1.info()))
+# print('About to start process 1')
+# sys.stdout.flush() 
 
-print('About to start process 1')
-sys.stdout.flush() 
-
-pm.run_process('GetLastDate')
+# pm.run_process('GetLastDate')
 
 
-from pprint import pprint
-for i in range(20):
-    pprint(pm.get_all_process_info())
-    sys.stdout.flush() 
-    sleep(2)
+# from pprint import pprint
+# for i in range(20):
+#     pprint(pm.get_all_process_info())
+#     sys.stdout.flush() 
+#     sleep(2)
 
 
 
